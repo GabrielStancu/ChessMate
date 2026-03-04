47ea640ee14e0fa42e0675eca55d17a889f2948b
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2026-03-04 22:19:46 +0200

    Update API base URL to use HTTPS for secure communication

--- src/frontend/chessmate-frontend/src/environments/environment.ts
+++ src/frontend/chessmate-frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://fa-chessmate-swn-001-e2bqe0dgemhjf5hp.switzerlandnorth-01.azurewebsites.net/api'
+  apiBaseUrl: 'https://fa-chessmate-swn-001-e2bqe0dgemhjf5hp.switzerlandnorth-01.azurewebsites.net/api'
 };
